710e44567cba029dd6da2202342ed1bf8cfadb64
xiexingen <1002275364@qq.com>
2020-02-26 02:14:34 +0000

    Deploying to gh-pages from  - 5d05e32ff892d9e6af30a70793a02b61b03a884e 🚀

---
+++ precache-manifest.f190ff00565c0fba537cce37339fc8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ceaeb04f1d4712d5629",
+    "url": "/umi.b5d1b60a.css"
+  },
+  {
+    "revision": "6ceaeb04f1d4712d5629",
+    "url": "/umi.0872990a.js"
+  },
+  {
+    "revision": "5a9283e6e72f9dc93a10db10a0ee789a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6523cc56219bba80a1196a69a0c4bc4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  }
+];
