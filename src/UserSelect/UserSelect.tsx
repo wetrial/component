4698818dc9da6a6ada2eb0b0cbb9f13f55f32713
daniel.su <daniel.su@ctsmed.com>
2020-07-28 11:57:51 +0800

    zc

--- src/UserSelect/UserSelect.tsx
+++ src/UserSelect/UserSelect.tsx
@@ -1,5 +1,34 @@
-import React, { PureComponent } from 'react';
-import { Avatar } from 'antd';
-import classNames from 'classnames';
+import React from 'react';
+import { Select } from 'antd';
+import Item from 'antd/lib/list/Item';
+// import classNames from 'classnames';
 
-export default AvatarList;
+interface UserSelectProps {
+  message?: string;
+  data?: Array<any>;
+  selectedData?: Array<any>;
+  UserInfo?: Array<any>;
+}
+const { Option, OptGroup } = Select;
+
+const UserSelect: React.FC<UserSelectProps> = (props) => {
+  const { data, selectedData, userInfo } = props;
+  return (
+    <div>
+      <Select
+        style={{ width: '200px' }}
+        dropdownRender={() => {
+          return userInfo.map((item) => {
+            return <div key={item.UserId}>{item.FullName}</div>;
+          });
+        }}
+      ></Select>
+    </div>
+  );
+};
+
+UserSelect.defaultProps = {
+  message: 'Hello',
+};
+
+export default UserSelect;
--- src/UserSelect/demo/demo1.tsx
+++ src/UserSelect/demo/demo1.tsx
@@ -1,6 +1,55 @@
 import React from 'react';
 import { UserSelect } from '@wetrial/component';
+const data = [
+  {
+    Id: '001',
+    Name: '张三',
+    Avatar: null,
+  },
+];
+
+const selectedData = ['001', '003'];
+
+const UserInfo = [
+  {
+    UserId: '001',
+    FullName: '张三',
+    RoleName: '管理员',
+    Avatar: null,
+  },
+  {
+    UserId: '002',
+    FullName: '李四',
+    RoleName: '秘书',
+    Avatar: null,
+  },
+  {
+    UserId: '003',
+    FullName: '王五',
+    RoleName: '研究者',
+    Avatar: null,
+  },
+  {
+    UserId: '004',
+    FullName: '令狐冲',
+    RoleName: '秘书',
+    Avatar: null,
+  },
+  {
+    UserId: '005',
+    FullName: '任我行',
+    RoleName: '秘书',
+    Avatar: null,
+  },
+];
 
 export default () => {
-  return null;
+  return (
+    <UserSelect
+      message="test text1"
+      data={data}
+      selectedData={selectedData}
+      userInfo={UserInfo}
+    ></UserSelect>
+  );
 };
