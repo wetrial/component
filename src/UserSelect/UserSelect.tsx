62bf33e2bc9d54b250b314eba171a4620e5f8b94
daniel.su <daniel.su@ctsmed.com>
2020-07-30 10:34:22 +0800

    zc

--- src/UserSelect/UserSelect.tsx
+++ src/UserSelect/UserSelect.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Select } from 'antd';
-import Item from 'antd/lib/list/Item';
-// import classNames from 'classnames';
+import { Select, Avatar } from 'antd';
+// import Item from 'antd/lib/list/Item';
+import classNames from 'classnames';
+import './index.less';
 
 interface UserSelectProps {
   message?: string;
@@ -16,15 +17,26 @@ const UserSelect: React.FC<UserSelectProps> = (props) => {
   return (
     <div>
       <Select
+        className="wt-select-default"
+        mode="tags"
         showSearch={true}
-        style={{ width: '200px' }}
-        // dropdownRender={() => {
-        // }}
+        style={{ width: 'fit-content', minWidth: '80px' }}
+        dropdownRender={(menu) => {
+          return (
+            <div>
+              {menu}
+              <div> footer</div>
+            </div>
+          );
+        }}
+        tagRender={(props) => {
+          return <Avatar>{props.label[1]}</Avatar>;
+        }}
       >
         {userInfo.map((item) => {
           return (
             <Option key={item.UserId} value={item.FullName}>
-              <span>Avatar</span>
+              <Avatar>{item.FullName}</Avatar>
               {item.FullName}
             </Option>
           );
